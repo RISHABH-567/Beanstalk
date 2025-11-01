3c75aface7cd8a7ee31e3fb9fba1c363478d23eb
RISHAB-567 <rishabgoyalsangrah@gmail.com>
2025-11-01 16:08:08 +0530

    Improved UI

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express = require("express")
 const port = process.env.PORT || 3000
 const app = express()
 
-// app.use(express.static('public'))
-app.get("/", (req, res)=>{
-    res.send("Changes are Available by CI/CD pipeline")
-})
+app.use(express.static('public'))
+
+
 
 app.listen(port, ()=>{
     console.log("Server is running")
