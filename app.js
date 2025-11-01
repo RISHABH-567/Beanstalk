91f122a72c685b89df8411d0c0a0f50f50887d6f
RISHAB-567 <rishabgoyalsangrah@gmail.com>
2025-11-01 13:02:36 +0530

    New Change to check Pipeline

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express")
 const port = process.env.PORT || 3000
 const app = express()
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
+app.get("/", (req, res)=>{
+    res.send("Changes are Available by CI/CD pipeline")
+})
 
 app.listen(port, ()=>{
     console.log("Server is running")
